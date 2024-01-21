349e2db9f414477aef4be6f121e81040583403f8
John Doe <john@email.com>
2024-01-21 13:46:56 +0200

    Price class is added

--- Lesson_02/oop.py
+++ Lesson_02/oop.py
@@ -2,3 +2,9 @@ class Person:
    def __init__(self, name, age) -> None:
       self.name = name
       self.age = age
+
+
+class Price:
+   def __init__(self, currency, value) -> None:
+      self.currency = currency
+      self.value = value 

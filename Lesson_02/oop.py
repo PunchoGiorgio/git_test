33307df6a70f00615fd5af4575c8c016b4ba01de
John Doe <john@email.com>
2024-01-22 15:36:53 +0200

    Some changes

--- Lesson_02/oop.py
+++ Lesson_02/oop.py
@@ -1,10 +1,11 @@
 class Person:
-   def __init__(self, name, age) -> None:
-      self.name = name
-      self.age = age
+   def __init__(self, name: str, age: int) -> None:
+      self.name: str = name
+      self.age: int = age
+
 
 
 class Price:
-   def __init__(self, currency: str, value: int) -> None:
-      self.currency: str = currency
-      self.value: int = value 
+   def __init__(self, currency, value) -> None:
+      self.currency = currency
+      self.value = value 

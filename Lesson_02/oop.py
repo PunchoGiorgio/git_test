6d5560605a05ae1bffd6e8f4c4d684197e8b1b66
George Pancheshenko <puncho_giorgio@inbox.lv>
2024-01-23 02:01:18 +0200

    changes

--- Lesson_02/oop.py
+++ Lesson_02/oop.py
@@ -1,11 +1,10 @@
 class Person:
-   def __init__(self, name: str, age: int) -> None:
-      self.name: str = name
-      self.age: int = age
-
+    def __init__(self, name: str, age: int) -> None:
+        self.name: str = name
+        self.age: int = age
 
 
 class Price:
-   def __init__(self, currency, value) -> None:
-      self.currency = currency
-      self.value = value 
+    def __init__(self, currency, value) -> None:
+        self.currency = currency
+        self.value = value
